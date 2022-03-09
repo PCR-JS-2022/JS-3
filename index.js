506c9b81c3a5380d19e4fb027ac2da590180a308
EgorNmv <43643833+EgorNmv@users.noreply.github.com>
2022-03-09 17:42:58 +0500

    Добавлены типы для функций

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
-function createClient(name, balance) {
+/**
+ * @typedef Client
+ * @type {object}
+ * @property {string} name
+ * @property {number} balance
+ */
 
-}
+/**
+ * @typedef Bank
+ * @type {object}
+ * @property {string} bankName
+ * @property {Array<Client>} clients
+ * @property {(client: Client) => boolean | Error} addClient
+ * @property {(client: Client) => boolean | Error} removeClient
+ */
 
-function createBank(bankName, clients) {
+/**
+ * @typedef Bankomat
+ * @type {object}
+ * @property {Bank} bank
+ * @property {{[key: string]: number}} notesRepository
+ * @property {Client | undefined} currentClient
+ * @property {(client: Client) => boolean} setClient
+ * @property {(client: Client) => boolean} removeClient
+ * @property {(notesRepository: {[key: string]: number}) => void} addMoney
+ * @property {(sumToGive: number) => boolean | Error} giveMoney
+ */
 
-}
+/**
+ * @name createClient
+ * @description Функция для создания клиента
+ * @param {string} name Имя клиента
+ * @param {number} balance Баланс клиента
+ * @returns {Client} Объект клиента
+ */
+function createClient(name, balance) {}
 
-function createBankomat(bankNotesRepository, bank) {
+/**
+ * @name createBank
+ * @description Функция для создания банка
+ * @param {bankName} name Имя банка
+ * @param {Array<Client>} clients Список клиентов банка
+ * @returns {Bank} Объект банка
+ */
+function createBank(bankName, clients) {}
 
-}
+/**
+ * @name createBankomat
+ * @description Фукнция для создания банкомата
+ * @param {{[key: string]: number}} bankNotesRepository Репозиторий валют
+ * @param {Bank} bank Объект банка
+ * @returns {Bankomat} Объект банкомата
+ */
+function createBankomat(bankNotesRepository, bank) {}
 
-module.exports = {createClient, createBank, createBankomat}
+module.exports = { createClient, createBank, createBankomat };
