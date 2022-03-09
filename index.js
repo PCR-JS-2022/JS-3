33bf0fba18e072f2bfdaf240f60e267e6bf55a71
Roman Khasimianov <roman.khasimyanov@gmail.com>
2022-03-09 15:39:48 +0500

    init index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+function createClient(name, balance) {
+
+}
+
+function createBank(bankName, clients) {
+
+}
+
+function createBankomat(bankNotesRepository, bank) {
+
+}
+
+module.exports = {createClient, createBank, createBankomat}
