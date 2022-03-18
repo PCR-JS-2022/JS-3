72c67758014cb36eb9a8162b49f225eb1c2b8f23
Svetomuzika <sozonov_artem@mail.ru>
2022-03-18 09:31:03 +0500

    Чисто затестить (ультрамегачерновой вариант)

--- index.js
+++ index.js
@@ -33,7 +33,27 @@
  * @param {number} balance Баланс клиента
  * @returns {Client} Объект клиента
  */
-function createClient(name, balance) {}
+
+function findClientIndex(clients, client) {
+    return clients.findIndex(e => e.name == client.name &&
+        e.balance == client.balance &&
+        typeof client.name == "string" &&
+        typeof client.balance == "number");
+}
+
+
+function createClient(name, balance) {
+    if (typeof name !== "string" || typeof balance !== "number") {
+        throw new Error;
+    }
+
+    const client = {
+        name,
+        balance,
+    };
+    return client;
+}
+
 
 /**
  * @name createBank
@@ -42,7 +62,42 @@ function createClient(name, balance) {}
  * @param {Array<Client>} clients Список клиентов банка
  * @returns {Bank} Объект банка
  */
-function createBank(bankName, clients) {}
+
+
+function createBank(bankName, clients) {
+    if (typeof bankName !== "string" || !Array.isArray(clients)) {
+        throw new Error;
+    }
+
+    const bank = {
+        bankName,
+        clients,
+
+        addClient(client) {
+            const checkClient = findClientIndex(this.clients, client);
+
+            if (checkClient == -1) {
+                this.clients.push(client);
+                return true;
+            }
+
+            throw new Error;
+        },
+
+        removeClient(client) {
+            const checkClient = findClientIndex(this.clients, client);
+
+            if (checkClient !== -1) {
+                this.clients.splice(checkClient, 1);
+                return true;
+            }
+
+            throw new Error;
+        },
+    }
+    return bank;
+}
+
 
 /**
  * @name createBankomat
@@ -51,6 +106,153 @@ function createBank(bankName, clients) {}
  * @param {Bank} bank Объект банка
  * @returns {Bankomat} Объект банкомата
  */
-function createBankomat(bankNotesRepository, bank) {}
+
+
+function createBankomat(bankNotesRepository, bank) {
+    if (typeof bank !== "object" || typeof bankNotesRepository !== "object")
+        throw new Error;
+
+    const bankomat = {
+        bank,
+        notesRepository: bankNotesRepository,
+        currentClient: undefined,
+
+        setClient(client) {
+
+            if (bankomat.currentClient == undefined && findClientIndex(this.bank.clients, client) !== -1) {
+                bankomat.currentClient = client;
+                return true;
+            }
+
+            throw new Error;
+        },
+
+        removeClient() {
+            if (bankomat.currentClient !== undefined) {
+                bankomat.currentClient = undefined;
+                return true;
+            }
+
+            throw new Error;
+        },
+
+        addMoney(...cashes) {
+            if (!this.currentClient)
+                throw new Error;
+
+            for (const cash of cashes) {
+                for (const e in cash) {
+                    this.notesRepository[e] += cash[e];
+                    this.currentClient.balance += cash[e] * e;
+                }
+            }
+            return this.addMoney.bind(this);
+        },
+
+        giveMoney(count) {
+            if (!this.currentClient || this.currentClient.balance < count || count % 10 !== 0) {
+                throw new Error;
+            }
+
+            const cash = count;
+            let rep = Object.entries(this.notesRepository).sort((a, b) => b[0] - a[0]);
+
+            const result = {};
+            let sum = 0;
+
+            for (let i = String(count).length; i > 1; i--) {
+                let e = Math.floor(count / Math.pow(10, i - 1));
+                e = e * Math.pow(10, i - 1);
+
+                rep.forEach(([banknote, banknoteCount], i) => {
+                    if (banknoteCount !== 0 && e >= banknote && banknoteCount * banknote >= e) {
+                        if (result[banknote]) {
+                            result[banknote] += Math.floor(e / banknote);
+                            rep[i] = [banknote, banknoteCount - Math.floor(e / banknote)];
+                            sum += banknote * Math.floor(e / banknote);
+                            e = e - banknote * Math.floor(e / banknote);
+                        } else {
+                            result[banknote] = Math.floor(e / banknote);
+                            rep[i] = [banknote, banknoteCount - Math.floor(e / banknote)];
+                            sum += banknote * Math.floor(e / banknote);
+                            e = e - banknote * Math.floor(e / banknote);
+                        }
+
+                    } else if (banknoteCount !== 0 && e >= banknote) {
+                        sum += banknote * banknoteCount;
+                        e = e - banknote * banknoteCount;
+                        if (result[banknote]) {
+                            result[banknote] += banknoteCount;
+                            rep[i] = [banknote, 0];
+                        } else {
+                            result[banknote] = banknoteCount;
+                            rep[i] = [banknote, 0];
+                        }
+                    }
+                });
+                count = count % Math.pow(10, i - 1);
+            }
+
+            if (sum != cash) {
+                throw new Error;
+            }
+            this.currentClient.balance -= sum;
+            return result;
+        }
+    }
+
+    return bankomat;
+
+}
+
+const notesRepository = {
+    5000: 5,
+    2000: 3,
+    1000: 0,
+    500: 0,
+    200: 0,
+    100: 0,
+    50: 2,
+    10: 300,
+};
+
+const clients = [
+    { name: 'чел', balance: 1488 },
+    { name: 'name 0', balance: 100 },
+    { name: 'name 1', balance: 101 },
+    { name: 'name 2', balance: 102 },
+    { name: 'name 3', balance: 103 },
+    { name: 'name 4', balance: 104 },
+    { name: 'name 5', balance: 105 },
+    { name: 'name 6', balance: 106 },
+    { name: 'name 7', balance: 107 },
+    { name: 'name 8', balance: 108 },
+    { name: 'name 9', balance: 109 },
+    { name: 'кент', balance: 2500000 },
+]
+
+const bank = createBank("Bibici", clients);
+const crtBank = createBankomat(notesRepository, bank);
+crtBank.setClient(createClient("кент", 2500000));
+//console.log(crtBank.addMoney({ 10: 2 }));
+// console.log(crtBank.addMoney({ 10: 2 })({ 50: 1, 10: 1 })({ 10: 3 }, { 100: 1 }));
+console.log(crtBank);
+console.log(crtBank.giveMoney(33330)); //1 150 000
+// console.log(crtBank);
+// console.log(crtBank);
+// console.log(bank.removeClient());
+// console.log(crtBank);
+// console.log(createClient("1212", 1450))
+// console.log(createBank(bnk, cl))
+// const cl1 = createClient("чел", 1487);
+// const cl2 = createClient("чел", 1488);
+// const crtBank = createBank("Bibici", clients);
+// console.log(crtBank);
+// console.log(crtBank.addClient(cl1));
+// console.log(crtBank);
+// console.log(crtBank.removeClient(cl2));
+// console.log(crtBank);
+
+
 
 module.exports = { createClient, createBank, createBankomat };
