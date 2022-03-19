abf87311b41313d8a841c421d98b7e04106338b1
Sharipov Aidar <Sharipow777@gmail.com>
2022-03-20 02:09:46 +0500

    Первая попытка

--- index.js
+++ index.js
@@ -33,7 +33,16 @@
  * @param {number} balance Баланс клиента
  * @returns {Client} Объект клиента
  */
-function createClient(name, balance) {}
+function createClient(name, balance) {
+    if (typeof name !== 'string' || typeof balance !== 'number') {
+        throw new Error('Invalid arguments given');
+    }
+
+    return {
+        name,
+        balance
+    }
+}
 
 /**
  * @name createBank
@@ -42,7 +51,43 @@ function createClient(name, balance) {}
  * @param {Array<Client>} clients Список клиентов банка
  * @returns {Bank} Объект банка
  */
-function createBank(bankName, clients) {}
+function createBank(bankName, clients = []) {
+    if (typeof bankName !== 'string' || !Array.isArray(clients)) {
+        throw new Error('Invalid arguments given');
+    }
+
+    return {
+        bankName,
+        clients: clients,
+        addClient: function(client) {
+            if (!isClient(client)) {
+                throw new Error('Invalid argument client given');
+            }
+
+            const alreadyExists = this.clients.some(bankClient => bankClient.name === client.name);
+            if (alreadyExists) {
+                throw new Error(`Client with name ${client.name} is already exists`);
+            }
+
+            this.clients.push(client);
+            return true;
+        },
+        removeClient: function(client) {
+            if (!isClient(client)) {
+                throw new Error('Invalid argument client given')
+            }
+
+            const prevLength = this.clients.length;
+            this.clients = this.clients.filter(bankClient => bankClient.name !== client.name);
+
+            if (this.clients.length === prevLength) {
+                throw new Error(`There is no client with name ${client.name}`)
+            }
+
+            return true;
+        }
+    }
+}
 
 /**
  * @name createBankomat
@@ -51,6 +96,126 @@ function createBank(bankName, clients) {}
  * @param {Bank} bank Объект банка
  * @returns {Bankomat} Объект банкомата
  */
-function createBankomat(bankNotesRepository, bank) {}
+function createBankomat(bankNotesRepository, bank) {
+    if (typeof bankNotesRepository !== 'object' || typeof bank !== 'object') {
+        throw new Error('Invalid arguments given');
+    }
 
+    const bankomat = {
+        bank,
+        notesRepository: bankNotesRepository,
+        currentClient: undefined,
+        setClient: function(client) {
+            if (this.currentClient !== undefined) {
+                throw new Error('There is already a client at the bankomat');
+            }
+
+            if (!isClient(client)) {
+                throw new Error('Invalid argument given');
+            }
+
+            this.currentClient = client;
+            return true;
+        },
+        removeClient: function(client) {
+            isCurrentClientExists();
+
+            if (!client || client.name !== this.currentClient.name) {
+                throw new Error('Wrong client given');
+            }
+
+            this.currentClient = undefined;
+            return true;
+        },
+        addMoney: function(money) {
+            isCurrentClientExists();
+
+            if (!money || typeof money !== 'object') {
+                throw new Error('Wrong money given');
+            }
+
+            const notes = Object.entries(money).map(el => [parseInt(el[0]), el[1]]);
+            const invalidMoney = notes.some(note => note[0] % 10 !== 0);
+            if (invalidMoney) {
+                throw new Error('Invalid money given');
+            }
+
+            let sum = 0;
+
+            for (const [note, noteAmount] of notes) {
+                sum += note * noteAmount;
+                if (!this.notesRepository[note]) {
+                    this.notesRepository[note] = noteAmount;
+                    continue;
+                }
+
+                this.notesRepository[note] += noteAmount;
+            }
+
+            this.currentClient.balance += sum;
+
+            return this.addMoney.bind(this);
+        },
+        giveMoney: function(money) {
+            isCurrentClientExists();
+
+            if (this.currentClient.balance < money) {
+                throw new Error('Current client balance is too low');
+            }
+
+            if (typeof money !== 'number' || money < 0 || money % 10 !== 0) {
+                throw new Error(`Invalid value given for argument money`);
+            }
+
+            let sum = 0;
+            const sortedNotesRepository = 
+                Object.entries(this.notesRepository)
+                .map(note => [parseInt(note[0]), note[1]])
+                .filter(note => note[0] <= money)
+                .sort((note1, note2) => note2[0] - note1[0]); //reverse sort
+
+            for (const [note, noteAmount] of sortedNotesRepository) {
+                const maxNoteAmount = Math.floor((money - sum) / note);
+                if (maxNoteAmount <= noteAmount) {
+                    sum+= note * maxNoteAmount;
+                }
+                else {
+                    sum += note * noteAmount;
+                }
+
+                if (sum === money) {
+                    break;
+                }
+            }
+
+            if (sum !== money) {
+                throw new Error(`Not enough money in bankomat to give ${money} money`);
+            }
+
+            this.currentClient.balance -= money;
+            return true;
+        }
+    }
+
+    const isCurrentClientExists = () => {
+        if (bankomat.currentClient === undefined) {
+            throw new Error('There is no client at bankomat');
+        }
+    }
+
+    return bankomat;
+}
+
+/**
+ * @returns {boolean} Является ли объект клиентом
+ */
+const isClient = (client) => {
+    if (!client || typeof client.balance !== 'number' || typeof client.name !== 'string') {
+        return false;
+    }
+
+    return true;
+}
+
+// eslint-disable-next-line no-undef
 module.exports = { createClient, createBank, createBankomat };
