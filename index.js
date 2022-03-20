7e57da4f4a3243a7d437844a348c06c96f447cb9
Tosikperm59 <tosikperm59@gmail.com>
2022-03-21 01:09:33 +0500

    15я попытка

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function createBank(bankName, clients = []) {
             if (clients.includes(client))
                 throw new UserException('Такой клиент уже имеется в банке');
 
-            clients = clients.concat(client) 
+            this.clients = this.clients.concat(client) 
             return true;
         },
     
