ac8317db6cc149d3a9512d3e9979172455076efb
Kisus228 <nik.babushkin.201408@gmail.com>
2022-03-22 01:35:20 +0500

    Первая попытка

--- index.js
+++ index.js
@@ -33,16 +33,44 @@
  * @param {number} balance Баланс клиента
  * @returns {Client} Объект клиента
  */
-function createClient(name, balance) {}
+function createClient(name, balance) {
+  if (typeof name === "string" && typeof balance === "number")
+    return { name, balance };
+  throw new Error("Ты втираешь мне какую-то дичь");
+}
 
 /**
  * @name createBank
  * @description Функция для создания банка
- * @param {bankName} name Имя банка
+ * @param {string} bankName Имя банка
  * @param {Array<Client>} clients Список клиентов банка
  * @returns {Bank} Объект банка
  */
-function createBank(bankName, clients) {}
+function createBank(bankName, clients) {
+  if (typeof bankName !== "string" || !Array.isArray(clients))
+    throw new Error("Ты снова втираешь мне какую-то дичь");
+
+  return {
+    bankName,
+    clients,
+    addClient: (newClient) => {
+      if (this.clients.some((client) => client.name === newClient.name))
+        throw new Error("Клиент с таким именем уже существует");
+      else {
+        this.clients.push(newClient);
+        return true;
+      }
+    },
+    removeClient: (clientToRemove) => {
+      if (this.clients.some((client) => client.name === clientToRemove.name)) {
+        this.clients = this.clients.filter(
+          (client) => client.name !== clientToRemove.name
+        );
+        return true;
+      } else throw new Error("Пытаемся удалить несуществующего клиента");
+    },
+  };
+}
 
 /**
  * @name createBankomat
@@ -51,6 +79,74 @@ function createBank(bankName, clients) {}
  * @param {Bank} bank Объект банка
  * @returns {Bankomat} Объект банкомата
  */
-function createBankomat(bankNotesRepository, bank) {}
+function createBankomat(bankNotesRepository, bank) {
+  if (
+    typeof bankNotesRepository !== "object" ||
+    !Object.prototype.hasOwnProperty.call(bank, "bankName") ||
+    !Object.prototype.hasOwnProperty.call(bank, "clients") ||
+    !Object.prototype.hasOwnProperty.call(bank, "addClient") ||
+    !Object.prototype.hasOwnProperty.call(bank, "removeClient")
+  )
+    throw new Error("Мне не нравится то, что ты мне втираешь");
+
+  const nominals = [10, 50, 100, 200, 500, 1000, 2000, 5000].reverse();
+
+  return {
+    bank,
+    notesRepository: bankNotesRepository,
+    currentClient: undefined,
+    setClient: (clientToSet) => {
+      if (
+        this.bank.clients.some((client) => client.name === clientToSet.name) &&
+        this.currentClient === undefined
+      ) {
+        this.currentClient = clientToSet;
+        return true;
+      } else
+        throw new Error(
+          "Пытаемся установить пользователя к банкомату банка, клиентом которого он не ялвяется, либо у банкомата уже стоит клиент"
+        );
+    },
+    removeClient: () => {
+      this.currentClient = undefined;
+      return true;
+    },
+    addMoney: (...moneysToAdd) => {
+      if (this.currentClient === undefined)
+        throw new Error("С капустой никто не работает");
+
+      moneysToAdd.forEach((moneyToAdd) => {
+        nominals.forEach((nominal) => {
+          if (moneyToAdd[nominal] !== undefined)
+            this.notesRepository[nominal] += moneyToAdd[nominal];
+        });
+      });
+      return this.addMoney;
+    },
+    giveMoney: (sumToGive) => {
+      if (sumToGive > this.currentClient.balance)
+        throw new Error("Сумма выдачи больше баланса клиента");
+      if (sumToGive % 10 !== 0) throw new Error("Сумма выдали не кратна 10");
+      if (this.currentClient === undefined)
+        throw new Error("С капустой никто не работает");
+
+      const moneyToGive = {};
+      nominals.forEach((nominal) => {
+        if (sumToGive === 0 || nominal > sumToGive) return;
+        const count = Math.min(
+          Math.trunc(sumToGive / nominal),
+          this.notesRepository[nominal]
+        );
+        sumToGive -= nominal * count;
+        this.notesRepository[nominal] -= count;
+        moneyToGive[nominal] = count;
+      });
+
+      if (sumToGive !== 0) throw new Error("Не хватило капусты");
+
+      return moneyToGive;
+    },
+  };
+}
 
 module.exports = { createClient, createBank, createBankomat };
