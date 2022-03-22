0af584238c4b372bda484d47e1ee6af049517b87
Михалев Кирилл <mihalov.k@mail.ru>
2022-03-22 22:43:00 +0500

    Первая попытка

--- index.js
+++ index.js
@@ -33,7 +33,11 @@
  * @param {number} balance Баланс клиента
  * @returns {Client} Объект клиента
  */
-function createClient(name, balance) {}
+function createClient(name, balance= 0) {
+    if (typeof name === "string" && typeof balance === "number")
+        return {name,balance}
+    else throw new Error("Ошибка ввода данных");
+}
 
 /**
  * @name createBank
@@ -42,7 +46,29 @@ function createClient(name, balance) {}
  * @param {Array<Client>} clients Список клиентов банка
  * @returns {Bank} Объект банка
  */
-function createBank(bankName, clients) {}
+function createBank(bankName, clients= []) {
+    if (typeof bankName === "string" && Array.isArray(clients)) {
+        return {
+            bankName,
+            clients,
+
+            addClient(client) {
+                if (!this.clients.find(elem => elem.name === client.name)) {
+                    clients.push(client);
+                    return true
+                } else throw new Error("Клиент не найден")
+            },
+
+            removeClient(client) {
+                const newArray = this.clients.filter(elem => elem.name !== client.name);
+                if (JSON.stringify(this.clients) !== JSON.stringify(newArray)) {
+                    this.clients = newArray;
+                    return true
+                } else throw new Error("Клиент не удален")
+            }
+        }
+    } else throw new Error("Ошибка создания банка");
+}
 
 /**
  * @name createBankomat
@@ -51,6 +77,75 @@ function createBank(bankName, clients) {}
  * @param {Bank} bank Объект банка
  * @returns {Bankomat} Объект банкомата
  */
-function createBankomat(bankNotesRepository, bank) {}
+function createBankomat(bankNotesRepository, bank) {
+    if (typeof bankNotesRepository === "object" && bank.hasOwnProperty("bankName")) {
+        return {
+            bank,
+            notesRepository: bankNotesRepository,
+            currentClient: undefined,
+
+            setClient(client) {
+                if (this.currentClient === undefined && this.bank.clients.find(elem => elem.name === client)) {
+                    this.currentClient = client;
+                    return true
+                } else throw new Error("Клиент не установлен")
+            },
+
+            removeClient() {
+                if (this.currentClient !== undefined && this.bank.clients.find(elem => elem.name === this.currentClient)) {
+                    this.currentClient = undefined;
+                    return true;
+                } else throw new Error("Клиент не удален")
+            },
+
+            addMoney(...banknotes) {
+                if (this.currentClient !== undefined && this.bank.clients.find(elem => elem.name === this.currentClient)) {
+                    banknotes.forEach(banknote => {
+                        for (let i in banknote) {
+                            this.notesRepository[i] += banknote[i];
+                            this.currentClient.balance += i * banknote[i];
+                        }
+                    })
+                } else throw new Error("Деньги не зачислены")
+            },
+
+            giveMoney(sum) {
+                if (this.currentClient !== undefined && this.bank.clients.find(elem => elem.name === this.currentClient)) {
+                    if (sum > this.currentClient.balance)
+                        throw new Error("Привышен лимит выдачи")
+                    if (sum % 10 !== 0)
+                        throw new Error("Сумма не кратна 10")
+
+                    const givenMoney = {
+                        5000: 0,
+                        2000: 0,
+                        1000: 0,
+                        500: 0,
+                        200: 0,
+                        100: 0,
+                        50: 0,
+                        10: 0
+                    }
+
+                    let sortedBankNotes = Object.entries(this.notesRepository).sort((a, b) => b[0] - a[0]);
+                    let currentSum = sum;
+                    for (let bankNote of sortedBankNotes) {
+                        if (bankNote[0] <= currentSum) {
+                            while (currentSum >= bankNote[0] && this.notesRepository[bankNote[0]] !== 0 && currentSum !== 0) {
+                                currentSum -= bankNote[0];
+                                this.notesRepository[bankNote[0]] -= 1;
+                                givenMoney[bankNote[0]] += 1
+                            }
+                        }
+                    }
+                    if (currentSum === 0) {
+                        this.currentClient.balance -= currentSum;
+                        return givenMoney
+                    } else throw new Error("Не хватает купюр");
+                } else throw new Error("Клиент не найден");
+            },
+        }
+    } else throw new Error("Невалиднын данные банка");
+}
 
 module.exports = { createClient, createBank, createBankomat };
