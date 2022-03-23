367f9d2ddfa17c4c3b17ebc251874474ee4bdd3c
danichka <daniika.ella@gmail.com>
2022-03-23 10:49:13 +0500

    ферст трай

--- index.js
+++ index.js
@@ -21,7 +21,7 @@
  * @property {{[key: string]: number}} notesRepository
  * @property {Client | undefined} currentClient
  * @property {(client: Client) => boolean} setClient
- * @property {(client: Client) => boolean} removeClient
+ *@property {(client: Client) => boolean} removeClient
  * @property {(notesRepository: {[key: string]: number}) => void} addMoney
  * @property {(sumToGive: number) => boolean | Error} giveMoney
  */
@@ -33,24 +33,142 @@
  * @param {number} balance Баланс клиента
  * @returns {Client} Объект клиента
  */
-function createClient(name, balance) {}
 
+ function createClient(name, balance = 0) {
+	return isClient ({ name, balance });
+}
 /**
  * @name createBank
  * @description Функция для создания банка
- * @param {bankName} name Имя банка
+ * @param {string} bankName Имя банка
  * @param {Array<Client>} clients Список клиентов банка
  * @returns {Bank} Объект банка
  */
-function createBank(bankName, clients) {}
-
+function createBank(bankName, clients = []) {
+	return isBank({
+		bankName,
+		clients,
+		addClient: function (client) {
+      if (!isClient(client) || this.clients.some(bankClient => bankClient === client))
+				throw new Error('Не удалось добавить клиента в банк');
+			this.clients.push(client);
+			return true;
+		},
+		removeClient: function (client) {
+      if (!isClient(client) || !this.clients.some(bankClient => bankClient === client))
+				throw new Error('Не удалось удалить клиента из банка');
+			this.clients = this.clients.filter(x => x !== client);
+			return true;
+		}
+	});
+}
 /**
  * @name createBankomat
- * @description Фукнция для создания банкомата
- * @param {{[key: string]: number}} bankNotesRepository Репозиторий валют
+ * @description Функция для создания банкомата
+ * @param {{[key: string]: number}} notesRepository Репозиторий валют
  * @param {Bank} bank Объект банка
  * @returns {Bankomat} Объект банкомата
  */
-function createBankomat(bankNotesRepository, bank) {}
+function createBankomat(notesRepository, bank) {
+	return isBankomat({
+		bank,
+		notesRepository,
+		currentClient: undefined,
+		setClient: function (client) {
+			if (this.currentClient !== undefined) 
+				throw new Error('В настоящий момент банкоматом пользуется другой клиент');
+			
+      if (!this.bank.clients.includes(client)) 
+				throw new Error('Пользователь не является клиентом банка');
+			
+			this.currentClient = client;
+			return true;
+		},
+
+		removeClient: function () {
+			this.currentClient = undefined;
+			return true;
+		},
+
+		addMoney: function (...money) {
+ 			if (!this.currentClient) 
+ 				throw new Error('Клиент не найден');
+ 			
+
+ 			for (const m of money) {
+ 				for (const nominal in m) {
+ 					this.notesRepository[nominal] += m[nominal];
+ 					this.currentClient.balance += m[nominal] * nominal;
+ 				}
+ 			}
+ 			return this.addMoney.bind(this);
+ 		},
+
+		giveMoney: function (money) {
+
+
+      if (!this.currentClient) 
+        throw new Error('Клиент не найден')
+
+			if (money % 10 !== 0) 
+				throw new Error('Недостаточно средств для снятия');
+			
+			if (money > this.currentClient.balance) {
+				throw new Error("Not enough money in the bank account");
+			}
+
+			const banknotes = [5000, 2000, 1000, 500, 200, 100, 50, 10];
+			const result = banknotes.reduce((result, banknote) => {
+				let boundedBanknotes = Math.floor(money / banknote);
+				const stockBanknotes = this.notesRepository[banknote];
+				if (boundedBanknotes === 0) {
+					return result;
+				}
+				if (stockBanknotes < boundedBanknotes) {
+					boundedBanknotes = stockBanknotes;
+				}
+				money -= banknote * boundedBanknotes;
+				this.notesRepository[banknote] -= boundedBanknotes;
+				result[banknote] = boundedBanknotes;
+				return result;
+			},)
+
+      if (money !== 0) 
+				throw Error('Не удалось выдать наличные. Недостаточно купюр.');
+
+			this.currentClient.balance -= money;
+			return result;
+		}
+	});
+}
+
+function isClient (client) {
+	if (!(typeof client === "object"
+		&& client.hasOwnProperty("name") && client.hasOwnProperty("balance")
+		&& client.name && typeof client.name === "string" 
+    && typeof client.balance === "number"	)) {
+		throw new Error("Invalid input data");
+	}
+	return client;
+}
+
+function isBank(bank) {
+	if (!(typeof bank === "object"
+		&& bank.hasOwnProperty("removeClient") && bank.hasOwnProperty("addClient")
+		&& bank.hasOwnProperty("clients") && bank.clients instanceof Array
+		&& bank.bankName && typeof bank.bankName === "string")) {
+		throw new Error('Не удалось создать банк');
+	}
+	return bank;
+}
+
+function isBankomat(bankomat) {
+	if (!(typeof bankomat === 'object'
+		&& isBank(bankomat.bank)
+		&& typeof bankomat.notesRepository === 'object')) {
+		throw new Error('Не удалось создать банкомат');
+	}
+	return bankomat;
+}
 
 module.exports = { createClient, createBank, createBankomat };
