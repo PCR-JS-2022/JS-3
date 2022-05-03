144c6988f74ea2841adfa2e77fe07407a832ff77
slay2me <slay96@me.com>
2022-05-04 00:27:38 +0500

    Test

--- index.js
+++ index.js
@@ -33,7 +33,16 @@
  * @param {number} balance Баланс клиента
  * @returns {Client} Объект клиента
  */
-function createClient(name, balance) {}
+function createClient(name, balance = 0) {
+    if (typeof name !== "string" || typeof balance !== "number") {
+        name = ""
+        balance = 0
+    }
+    return {
+        name: name,
+        balance: balance
+    }
+}
 
 /**
  * @name createBank
@@ -42,7 +51,36 @@ function createClient(name, balance) {}
  * @param {Array<Client>} clients Список клиентов банка
  * @returns {Bank} Объект банка
  */
-function createBank(bankName, clients) {}
+function createBank(bankName, clients = []) {
+    function addClient(client) {
+        if (typeof client !== "object") {
+            return TypeError("Ожидается тип object")
+        }
+        if (clients.includes(client)) {
+            return Error("Данный клиент уже есть в списке клиентов банка")
+        }
+        clients.push(client)
+        return true
+    }
+
+    function removeClient(client) {
+        if (typeof client !== "object") {
+            return TypeError("Ожидается тип object")
+        }
+        if (!clients.includes(client)) {
+            return Error("Данного клиента ещё нет в списке клиентов банка")
+        }
+        clients.splice(clients.indexOf(client), 1)
+        return true
+    }
+
+    return {
+        bankName: bankName,
+        clients: clients,
+        addClient: addClient,
+        removeClient: removeClient
+    }
+}
 
 /**
  * @name createBankomat
@@ -51,6 +89,97 @@ function createBank(bankName, clients) {}
  * @param {Bank} bank Объект банка
  * @returns {Bankomat} Объект банкомата
  */
-function createBankomat(bankNotesRepository, bank) {}
+function createBankomat(bankNotesRepository, bank) {
+    if (typeof bankNotesRepository !== "object" || typeof bank !== "object") {
+        console.log(Error(""))
+    }
+
+    let currentClient = undefined
+
+    function setClient(client) {
+        if (!bank.clients.includes(client)) {
+            return Error("Данного клиента ещё нет в списке клиентов банка")
+        }
+        if (currentClient !== undefined) {
+            return Error("Банкомат занят")
+        }
+        currentClient = client
+        return true
+    }
+
+    function removeClient() {
+        if (currentClient == undefined) {
+            return Error("Банкомат и так пуст")
+        }
+        currentClient = undefined
+        return true
+    }
+
+    function addMoney(...bankNotes) {
+        if (typeof bankNotes !== "object" || typeof currentClient == undefined) {
+            return Error("Нет текущего клиента")
+        }
+
+        nextAddMoney(...bankNotes)
+
+        function nextAddMoney(...bankNotes) {
+            Object.keys(bankNotesRepository).forEach(bankKey => {
+                bankNotes.forEach(bankNote => {
+                    Object.keys(bankNote).forEach(clientKey => {
+                        if (bankKey == clientKey) {
+                            bankNotesRepository[bankKey] += bankNote[clientKey]
+                            currentClient.balance += clientKey * bankNote[clientKey]
+                        }
+                    })
+                })
+            })
+            return nextAddMoney
+        }
+
+        return nextAddMoney
+    }
+
+    function giveMoney(money) {
+        if (typeof currentClient == "undefined") {
+            return Error("Нет текущего клиента")
+        }
+        if (money <= 0) {
+            return Error("Введите сумму больше 0")
+        }
+        if (money > currentClient.balance) {
+            return Error("Недостаточно средств на балансе")
+        }
+        if (money % 10 != 0) {
+            return Error("Введите сумму кратную 10")
+        }
+        let bankNotes = {}
+        Object.keys(bankNotesRepository)
+        .reverse()
+        .forEach(bankKey => {
+            while (money - bankKey >= 0 && bankNotesRepository[bankKey] > 0) {
+                if (bankNotes[bankKey] == undefined) {
+                    bankNotes[bankKey] = 0
+                }
+                bankNotes[bankKey] += 1
+                bankNotesRepository[bankKey] -= 1
+                money -= bankKey
+            }
+        })
+        if (money != 0) {
+            return Error("В банкомате не хватает купюр")
+        }
+        return bankNotes
+    }
+
+    return {
+        bank: bank,
+        notesRepository: bankNotesRepository,
+        currentClient: currentClient,
+        setClient: setClient,
+        removeClient: removeClient,
+        addMoney: addMoney,
+        giveMoney: giveMoney
+    }
+}
 
 module.exports = { createClient, createBank, createBankomat };
