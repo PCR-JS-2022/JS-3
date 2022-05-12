3476ef22d1fb63b9a7728952b2913cbe6bab83eb
lena <vasyasina.lena11@gmail.com>
2022-05-12 20:04:05 +0500

    таск дане

--- index.js
+++ index.js
@@ -33,7 +33,13 @@
  * @param {number} balance Баланс клиента
  * @returns {Client} Объект клиента
  */
-function createClient(name, balance) {}
+function createClient(name, balance = 0) {
+    if (typeof name !== 'string' || typeof balance !== 'number') {
+        throw new Error('Неверные входные данные');
+    }
+
+    return {name, balance};
+}
 
 /**
  * @name createBank
@@ -42,7 +48,32 @@ function createClient(name, balance) {}
  * @param {Array<Client>} clients Список клиентов банка
  * @returns {Bank} Объект банка
  */
-function createBank(bankName, clients) {}
+function createBank(bankName, clients = []) {
+    if (typeof bankName !== 'string') {
+        throw new Error('Неверные входные данные');
+    }
+
+    function addClient(name, balance) {
+        if (clients.find((client) => client.name === name)) {
+            return false;
+        }
+
+        clients.push(createClient(name, balance));
+
+        return true;
+    }
+
+    function removeClient(name) {
+        if (clients.find((client) => client.name === name)) {
+            clients = clients.filter((client) => client.name === name)
+            return true;
+        }
+
+        return false;
+    }
+
+    return {bankName, clients, addClient, removeClient};
+}
 
 /**
  * @name createBankomat
@@ -51,6 +82,76 @@ function createBank(bankName, clients) {}
  * @param {Bank} bank Объект банка
  * @returns {Bankomat} Объект банкомата
  */
-function createBankomat(bankNotesRepository, bank) {}
+function createBankomat(bankNotesRepository, bank) {
+    if (!bank) {
+        throw Error("Неверные входные данные");
+    }
 
-module.exports = { createClient, createBank, createBankomat };
+    let currentClient = undefined;
+
+    return {
+        bank,
+        notesRepository: bankNotesRepository,
+        currentClient: currentClient,
+        setClient(client) {
+            if (client === undefined){
+                throw Error("Такого клиента нету");
+            }
+
+            if (!bank.clients.find((bc) => bc.name === client.name)) {
+                throw Error("Такого клиента нету");
+            }
+
+            this.currentClient = client;
+            return true;
+        },
+        removeClient() {
+            currentClient = undefined;
+            return true;
+        },
+        addMoney(...money) {
+            if (!this.currentClient) {
+                throw new Error('Текущий пользователь нету');
+            }
+
+            for (let elem of money) {
+
+                const [[nominal, count]] = Object.entries(elem);
+                this.currentClient.balance += (+nominal) * (count);
+                if (this.notesRepository[nominal]) {
+                    this.notesRepository[nominal] += count;
+                } else {
+                    this.notesRepository[nominal] = count;
+                }
+            }
+
+            return this.addMoney.bind(this);
+        },
+        giveMoney(money) {
+            const moneyCopy = money;
+            if (!this.currentClient) {
+                throw new Error('Текущий пользователь нету');
+            }
+
+            if (money > this.currentClient.balance) {
+                throw new Error('Бабок нету, грустна');
+            }
+
+            const res = {}
+            for (let i of Object.keys(this.notesRepository).sort((a,b) => -a+b)) {
+                if (money > +i) {
+                    const count = Math.min(Math.floor(money / +i), this.notesRepository[i]);
+                    this.notesRepository[i] -= count;
+                    money -= count * +i;
+                    res[i] = count;
+                }
+            }
+
+            this.currentClient.balance -= moneyCopy;
+            return res;
+        },
+    }
+}
+
+
+module.exports = {createClient, createBank, createBankomat};
